d7d3afc05b8dc3b55d471dee732372608aed8af3
Spoody <balls93729@gmail.com>
2024-12-31 04:47:54 -0800

    Bump version to S-101.2

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local VERSION = "S-101.1"
+local VERSION = "S-101.2"
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
